6ed8733eeb76a1c0cde21c3e8dd05fef652bf000
riyanris <riyanrisky129@gmail.com>
2022-10-25 21:02:57 +0700

    route done

--- app/Views/home.php
+++ app/Views/home.php
@@ -3,6 +3,9 @@ $cfg = new \SConfig();
 ?>
 <?=view('templates/head')?>
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin="" />
+<link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
+<link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.76.0/dist/L.Control.Locate.min.css" />
 
 <?=view('templates/nav')?>
 
@@ -16,6 +19,9 @@ $cfg = new \SConfig();
 <?=view('templates/foot')?>
 
 <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
+<script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
+<script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.76.0/dist/L.Control.Locate.min.js" charset="utf-8"></script>
 
     <script>
       var map = L.map('map').setView([-0.712412, 119.975704], 12);
@@ -30,6 +36,13 @@ $cfg = new \SConfig();
         L.marker(['<?=$key['latitude']?>', '<?=$key['longitude']?>']).addTo(map)
           .bindPopup('<?=$judul?>');
         <?php } ?>
+
+        L.Routing.control({
+            routeWhileDragging: true,
+            geocoder: L.Control.Geocoder.nominatim()
+        }).addTo(map);
+        
+       L.control.locate().addTo(map);
     </script>
 
       
